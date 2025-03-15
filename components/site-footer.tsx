cbb1dd91b081b513f2ab0e860cd57b4a8c52f70c
JustQiyi <justqiyi@qq.com>
2025-03-15 17:31:11 +0800

    主要UI完成.

---
+++ components/site-footer.tsx
@@ -0,0 +1,13 @@
+export function SiteFooter() {
+  return (
+    <footer className="border-grid border-t py-6 md:py-0">
+      <div className="container-wrapper">
+        <div className="container py-4">
+          <div className="flex text-balance text-center text-sm leading-loose text-muted-foreground md:text-left justify-center">
+            Copyright 2022-2025 Cloudery Studio, All rights reserved.
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
